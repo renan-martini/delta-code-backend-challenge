d09c2098ba4f6d62cda33bf3cd8fd5873f212575
renan-martini <renan.martiniduarte@gmail.com>
2023-05-11 15:42:18 -0300

    fix: retrieve de user não pede mais o id para o usuário receber apenas seu proprio perfil

--- src/services/User/retrieve.service.ts
+++ src/services/User/retrieve.service.ts
@@ -1,11 +1,15 @@
 import AppDataSource from "../../data-source";
 import { User } from "../../entities/user.entity";
 import { NotFoundError } from "../../errors/NotFoundError";
+import { MyContext } from "../../interfaces/context.interfaces";
 import { dateFormatter } from "../../utils/dateFormatter.utils";
-import { validateUUID } from "../../utils/uuid.utils";
 
-export const retrieveUser = async (_: any, { id }: { id: string }) => {
-  validateUUID(id);
+export const retrieveUser = async (
+  _: any,
+  __: any,
+  { validate }: MyContext
+) => {
+  const id = validate();
   const userRepository = AppDataSource.getRepository(User);
   const user = await userRepository.findOneBy({ id, isActive: true });
   if (!user) throw new NotFoundError("User not found");
